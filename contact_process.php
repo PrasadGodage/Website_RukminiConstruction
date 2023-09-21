feac44653f966c9585cc93f237747ecdb9993a86
GauraVanam18 <132244932+GauraVanam18@users.noreply.github.com>
2023-09-21 14:26:13 +0530

    committed by gaurav

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "soulsoft.gauravvanam@gmail.com";
+    $to = "info@rukminiconstruction.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
