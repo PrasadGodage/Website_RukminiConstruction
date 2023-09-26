e1d273fad1c508507428047e79eaec23530dfd31
SoulsoftAmol <141608229+SoulsoftAmol@users.noreply.github.com>
2023-09-26 16:37:59 -0700

    commited by amol

--- email.php
+++ email.php
@@ -10,10 +10,10 @@
         // $subject = trim($_POST["subject"]);
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        // $phone = trim($_POST["phone"]);
+        $subject = trim($_POST["subject"]);
         $message = trim($_POST["message"]);
         
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)  OR empty($message)) {
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -23,7 +23,7 @@
         # Mail Content
         $content = "Name: $name\n";
         $content .= "Email: $email\n\n";
-        // $content .= "Phone: $phone\n";
+        $subject .= "subject: $subject\n";
         $content .= "Message: $message\n";
         $subject = " Rukmini Construction";
         # email headers.
