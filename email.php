18724085448d9940fc91104c90d6a1561f6e0929
SoulsoftAmol <141608229+SoulsoftAmol@users.noreply.github.com>
2023-09-26 18:33:34 -0700

    commited by amol

--- email.php
+++ email.php
@@ -33,16 +33,16 @@
         if ($success) {
             # Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            json_decode("Thank You! Your message has been sent.");
         } else {
             # Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
+			json_decode("Oops! Something went wrong, we couldn't send your message.");
         }
 
     } else {
         # Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+		json_decode("There was a problem with your submission, please try again.");
     }
 ?>
